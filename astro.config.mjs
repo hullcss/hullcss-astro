16be6224f9748f0229cecedbf04790b7349bd1fa
Ash Entwisle <ash.entwisle@protonmail.com>
2023-09-13 01:07:52 +0000

    added more boilerplate
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), mdx(), partytown(), sitemap(), prefetch()],
+  output: "server",
+  adapter: cloudflare()
+});

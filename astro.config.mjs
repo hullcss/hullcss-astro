d00f370ee1738c963ec4e4b3cbc4a9c89cb278bb
Ash Entwisle <ash.entwisle@protonmail.com>
2023-11-09 14:15:00 +0000

    big update
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,11 +10,19 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hullcss.org",
-  integrations: [tailwind(), mdx(), partytown(), sitemap(), prefetch()],
+  integrations: [
+    tailwind(), 
+    mdx(), 
+    partytown(), 
+    sitemap(), 
+    prefetch()
+  ],
   output: "server",
   adapter: cloudflare(),
   redirects: {
+    "/posts": "/blog",
     "/links": "https://linktr.ee/hullcss",
+    "/links/test": "https://youtu.be/dQw4w9WgXcQ",
     "/links/discord": "https://discord.gg/AFxbUs5kyA",
     "/links/facebook": "https://facebook.com/hullCSS",
     "/links/github": "https://github.com/hullcss",
@@ -22,5 +30,10 @@ export default defineConfig({
     "/links/instagram": "https://instagram.com/hull_css/",
     "/links/linkedin": "https://linkedin.com/company/hullcss",
     "/links/twitter": "https://twitter.com/hull_CSS",
+    "/links/email": "mailto:hullcss@hull.ac.uk",
+    "/links/website": "https://hullcss.org",
+    "/links/address": "https://what3words.com/torch.dips.factor",
+    "/links/freeside": "https://freeside.co.uk",
+    "/links/gradcracker": "https://www.gradcracker.com/student-societies/4305/hull-computer-science-society-css"
   }
 });

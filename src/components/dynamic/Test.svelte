3020a2d693e79cb627c1c09596ebebf85ae04a63
Ash Entwisle <ash.entwisle@protonmail.com>
2024-02-20 14:34:34 +0000

    added component library from other project
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

---
+++ src/components/dynamic/Test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let items: string[] = [];
+  
+  let text = items[0];
+</script>
+
+<span>{text}</span>
---
+++ src/components/dynamic/TextTyper.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount, onDestroy } from 'svelte';
+
+  export let text: string = '';
+
+
+  onMount(() => {
+    // on page load, type the text in the span
+    const textTyper = document.querySelector('.text_typer')!;
+    let i = 0;
+    const speed = 25;
+
+    function typeWriter() {
+      if (i < text.length) {
+        textTyper.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+
+    typeWriter();
+  });
+
+</script>
+
+<span class="text_typer"></span>

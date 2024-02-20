bbc30b9cad9eb4c39fe5fc9f8a82e738ae1509c8
Ash Entwisle <ash.entwisle@protonmail.com>
2024-02-20 14:37:19 +0000

    updated config files
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,16 @@ import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
+import svelte from "@astrojs/svelte";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hullcss.org",
-  integrations: [tailwind(), mdx(), partytown(), sitemap(), prefetch()],
   output: "server",
-  adapter: cloudflare(),
-  redirects: {
-    "/links": "https://linktr.ee/hullcss",
-    "/links/discord": "https://discord.gg/AFxbUs5kyA",
-    "/links/facebook": "https://facebook.com/hullCSS",
-    "/links/github": "https://github.com/hullcss",
-    "/links/husu": "https://hulluniunion.com/activities/view/ComputerScience",
-    "/links/instagram": "https://instagram.com/hull_css/",
-    "/links/linkedin": "https://linkedin.com/company/hullcss",
-    "/links/twitter": "https://twitter.com/hull_CSS",
-  }
+  integrations: [tailwind({
+    configFile: "./tailwind.config.cjs"
+  }), mdx(), partytown(), sitemap(), prefetch(), svelte()],
+  site: "https://hullcss.org",
+  adapter: cloudflare()
 });
